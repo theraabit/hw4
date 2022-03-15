6e600f935aec1ab7c12f0674f7d1c71b21bb8b85
theraabit <mike.raab@kellogg.northwestern.edu>
2022-03-15 16:08:03 +0000

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.where({ email: params["email"] })[0]
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        redirect_to "/"
+      else
+        flash[:notice] = "Please try again."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Please try again"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to "/sessions/new"
   end
 end
   
